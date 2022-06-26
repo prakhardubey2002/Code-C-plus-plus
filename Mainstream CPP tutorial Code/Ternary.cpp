1b3ba6c5efe2141397932c59ba8901200b3ab7fe
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-06-26 21:33:05 +0530

    Ternary operator added

---
+++ Mainstream CPP tutorial Code/Ternary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a Number: ";
+    cin>>num;
+    (num%2==0)?(cout<<"\nEven Number"):(cout<<"\nOdd Number");
+    cout<<endl;
+    return 0;
+}
