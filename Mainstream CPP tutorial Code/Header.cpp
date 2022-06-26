a744ce679bad083980ca885c342540d88527c8b4
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-06-27 00:32:11 +0530

    header file added

---
+++ Mainstream CPP tutorial Code/Header.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Sum.h"
+using namespace std;
+int main(){
+    int a=8,b=4;
+    hello();
+    sum(a,b);
+    return 0;
+}
---
+++ Mainstream CPP tutorial Code/Sum.h
@@ -0,0 +1,6 @@
+void hello(){
+    cout<<"Hello World"<<endl;
+}
+int sum(int a,int b){
+    cout<<"Sum of given two integer is : "<<a+b<<endl;
+}
