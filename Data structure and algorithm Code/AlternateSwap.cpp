814f3bcc9b4d7f9930cb0a8f8e11f41ee853209a
Prakhar <prakhardubey2002@gmail.com>
2022-09-04 00:24:57 +0530

    Alternateswap added

---
+++ Data structure and algorithm Code/AlternateSwap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"";
+    }cout <<endl;
+}
+void swapAlternate(int arr[],int size){
+    for(int i =0;i<size;i+=2){
+        if(i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+int main(){
+    int even[]={1,2,3,4};
+    int odd[]={1,2,3,4,5};
+    swapAlternate(even,sizeof(even)/sizeof(int));
+    print(even,sizeof(even)/sizeof(int));
+    return 0;
+}
