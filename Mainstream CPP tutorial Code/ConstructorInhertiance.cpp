f7d89477a26dc0223fc65e45096fa4c0ce16846a
Prakhar <prakhardubey2002@gmail.com>
2022-10-15 01:05:06 +0530

    inhertiance constructor added

---
+++ Mainstream CPP tutorial Code/ConstructorInhertiance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+we can use constructor in derived class in c++
+if base class constructor does not have any arguments,ther is no need of any constructor in derived class.
+BUt if ther are one or more argument in the base class constructor,derived class need to pass argument to the base class constructor 
+if both base and derived class have constructor ,base class constructor is executed first.
+The cosntstuctor of virtual base class is given preference over non virtusl class
+if multiple virtual class are declared then they will execute in  order of declarstion
+*/
+int main(){
+
+    return 0;
+}
