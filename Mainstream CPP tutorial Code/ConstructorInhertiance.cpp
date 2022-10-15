16476a0eac15c216712f2bf88fc3e467ad7d3261
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-10-16 00:33:31 +0530

    i=readme added

--- Mainstream CPP tutorial Code/ConstructorInhertiance.cpp
+++ Mainstream CPP tutorial Code/ConstructorInhertiance.cpp
@@ -8,7 +8,44 @@ if both base and derived class have constructor ,base class constructor is execu
 The cosntstuctor of virtual base class is given preference over non virtusl class
 if multiple virtual class are declared then they will execute in  order of declarstion
 */
+class Base1{
+    int data1;
+    public :
+        Base1(int i){
+            data1=i;
+            cout<<"The Base1 constructor called "<<endl;
+        }
+        void printData1(void){
+            cout<<"The value of data1 is "<<data1<<endl;
+        }
+};
+class Base2{
+    int data2;
+    public :
+        Base2(int i){
+            data2=i;
+            cout<<"The Base1 constructor called "<<endl;
+        }
+        void printData2(void){
+            cout<<"The value of data2 is "<<data2<<endl;
+        }
+};
+class Derived:public Base1,public Base2 {
+    int derived1,derived2;
+    public:
+    Derived(int a,int b,int c ,int d):Base1(a),Base2(b){
+        derived1=c;
+        derived2=d;
+        cout<<"Derive class condstrutor called"<<endl;
+    }
+    void printdata(void){
+      cout<<"The value of derived1 is "<<derived1<<endl;
+      cout<<"The value of derived2 is "<<derived2<<endl;
+    }
+};
 int main(){
-
+    Derived Prakhar(1,2,3,4);
+    Prakhar.printData1();
+    Prakhar.printData2();
     return 0;
 }
---
+++ Readme.md
@@ -0,0 +1,20 @@
+## Constructor
+we can use constructor in derived class in c++
+if base class constructor does not have any arguments,ther is no need of any constructor in derived class.
+BUt if ther are one or more argument in the base class constructor,derived class need to pass argument to the base class constructor 
+if both base and derived class have constructor ,base class constructor is executed first.
+The cosntstuctor of virtual base class is given preference over non virtusl class
+if multiple virtual class are declared then they will execute in  order of declarstion
+## Order of execution
+Case 1 :
+    class B: public A{
+        order of execution of constructor: first A() then B()
+    };
+Case 2 :
+    class A:public B,public C{
+        order of execution of constructor:first b()then C() then A()
+    }
+Case 3 :
+    class A:public B,virtual public C{
+        order of execution of constructor : C() then B() then A()
+    }
